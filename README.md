39fca939977e189a003515e02d5918a04c1a484e
desperu <motais.esteban@laposte.net>
2019-10-01 23:38:42 +0200

    First commit create project

---
+++ README.md
@@ -0,0 +1,5 @@
+My News!
+
+A simple application to read New York Times newspapers articles. There's three tabs in myNews, Top Stories, Most Popular and Business. You can search an article with yours personal preferences. You can set notifications to knows when a new article with yours preferences is post.
+
+Enjoy !
---
+++ app/src/main/java/org/desperu/mynews/ui/main/PageViewModel.java
@@ -0,0 +1,39 @@
+package org.desperu.mynews.ui.main;
+
+// Log test
+import android.util.Log;
+
+import androidx.arch.core.util.Function;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+import androidx.lifecycle.ViewModel;
+
+public class PageViewModel extends ViewModel {
+    // TODO delete log
+    // Log test
+    private static final String TAG = PageViewModel.class.getSimpleName();
+
+    private MutableLiveData<Integer> mIndex = new MutableLiveData<>();
+    private LiveData<String> mText = Transformations.map(mIndex, new Function<Integer, String>() {
+        @Override
+        public String apply(Integer input) {
+            // Log test
+            Log.d(TAG, "apply() called with: input = [" + input + "]");
+
+            return "Hello world from section: " + input;
+        }
+    });
+
+    public void fuckTravisbug() {
+        String travis ="Fuck it";
+    }
+
+    public void setIndex(int index) {
+        mIndex.setValue(index);
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
